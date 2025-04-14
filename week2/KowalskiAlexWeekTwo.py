f2f8dc235249c7411a3818e3db2523d207a4e339
Alex Kowalski <akowalski2@cocc.edu>
2025-04-13 17:48:24 -0700

    finish week 2

--- week2/KowalskiAlexWeekTwo.py
+++ week2/KowalskiAlexWeekTwo.py
@@ -1,5 +1,20 @@
+import matplotlib.pyplot as plot
+
 def main():
-    print("Hello, World!");
+    """
+    Main Entry Point
+    """
+    print("Alex Kowalski");
+    # set up your lists
+    numlist = [8, 6, 5, 3]
+    namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
+    colorlist = ['green', 'red', 'yellow', 'pink' ]
+    explodelist = [0.02, 0.02, 0.02, 0.02]
+    # make the pie chart
+    plot.pie(numlist, labels=namelist, autopct='%.1f%%', colors=colorlist,
+             explode = explodelist, startangle = 225)
+    plot.axis('equal')
+    plot.savefig('week2/piechart.png')
 
 if __name__ == "__main__":
     main()
