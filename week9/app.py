f1fc6579359fe413acc4808218fa3188a9c1e9be
Alex Kowalski <akowalski2@cocc.edu>
2025-06-12 23:56:29 -0700

    finish week 9

--- week9/app.py
+++ week9/app.py
@@ -5,10 +5,13 @@ app = Flask(__name__);
 @app.route("/about")
 def about():
     url_for("static", filename="styles.css")
+    url_for("static", filename="mind-blow-galaxy.gif")
     return render_template("about.html");
 
 @app.route("/")
 def home():
     url_for("static", filename="styles.css")
+    url_for("static", filename="hubble-ultra-deep-field.webp")
     return render_template("index.html");
+
 
